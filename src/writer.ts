0ba2bdf81d8961793186d9545e02556d6a6e829e
alvaro-escalante <a.fernandez.naves@gmail.com>
2023-06-30 11:12:12 +0200

    Enclose values with quotes if they contain commas

--- src/writer.ts
+++ src/writer.ts
@@ -12,8 +12,17 @@ export default async (dest: string, data: any[]) => {
     writeStream.write(`${headers}\n`) // Write the header line
 
     for (const row of data) {
-      const line = Object.values(row).join(',')
-      writeStream.write(`${line}\n`) // Write each data row
+      const line = Object.values(row)
+        .map((value) => {
+          // Enclose values with quotes if they contain commas
+          if (typeof value === 'string' && value.includes(',')) {
+            return `"${value}"`
+          }
+          return value
+        })
+        .join(',')
+
+      writeStream.write(`${line}\n`)
     }
 
     writeStream.end() // Close the write stream
